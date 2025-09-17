c6c4b7d026b656ce11bec4a6df558896e2c4d79b
PrashantKumar-PKLTD <connect@prashantkumar.co.uk>
2025-09-18 00:54:28 +0530

    Generate All Service Pages

--- src/screens/Content/services/AIPage.tsx
+++ src/screens/Content/services/AIPage.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { FloatingCTA } from "../../../components/ui/floating-cta";
+import { ContactFooterSection } from "./ContactFooterSection";
+import { MainContentSection } from "./MainContentSection";
+import { ProjectCallToActionSection } from "./ProjectCallToActionSection";
+import AIHero from "./AIHero";
 
-const AIPage = () => {
+export const Home = (): JSX.Element => {
   return (
-    <div>AIPage</div>
-  )
-}
+    <main
+      className="relative w-full min-h-screen bg-white overflow-hidden"
+      data-model-id="42:33829"
+    >
+      {/* Decorative background lines */}
+      <div className="absolute inset-0 flex justify-center">
+        <div className="relative w-full max-w-[1312px] h-full">
+          <div
+            className="absolute top-1/2 left-1/2 w-[5371px] h-px bg-[#90919c29] -rotate-90 opacity-75 -translate-x-1/2 -translate-y-1/2"
+            style={{ left: "calc(50% - 840px)" }}
+          />
+          <div
+            className="absolute top-1/2 left-1/2 w-[5371px] h-px bg-[#90919c29] -rotate-90 opacity-75 -translate-x-1/2 -translate-y-1/2"
+            style={{ left: "calc(50% - 504px)" }}
+          />
+          <div
+            className="absolute top-1/2 left-1/2 w-[5371px] h-px bg-[#90919c29] -rotate-90 opacity-75 -translate-x-1/2 -translate-y-1/2"
+            style={{ left: "calc(50% - 168px)" }}
+          />
+          <div
+            className="absolute top-1/2 left-1/2 w-[5371px] h-px bg-[#90919c29] -rotate-90 opacity-75 -translate-x-1/2 -translate-y-1/2"
+            style={{ left: "calc(50% + 168px)" }}
+          />
+          <div
+            className="absolute top-1/2 left-1/2 w-[5371px] h-px bg-[#90919c29] -rotate-90 opacity-75 -translate-x-1/2 -translate-y-1/2"
+            style={{ left: "calc(50% + 471px)" }}
+          />
+        </div>
+      </div>
 
-export default AIPage
+      {/* Main content sections with entrance animations */}
+      <div className="relative z-10">
+        <section className="w-full translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:0ms]">
+          <AIHero />
+          <MainContentSection />
+        </section>
+
+        <section className="w-full translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+          <ProjectCallToActionSection />
+        </section>
+
+        <section className="w-full translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+          <ContactFooterSection />
+        </section>
+      </div>
+
+      {/* Floating CTA */}
+      <FloatingCTA />
+    </main>
+  );
+};
+
+export default Home;
