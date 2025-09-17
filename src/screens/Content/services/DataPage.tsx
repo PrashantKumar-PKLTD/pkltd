a04190761af0765503a8b465a5ef281a0c5869ac
PrashantKumar-PKLTD <connect@prashantkumar.co.uk>
2025-09-18 01:32:41 +0530

    Fix DataPage to match other service pages structure

--- src/screens/Content/services/DataPage.tsx
+++ src/screens/Content/services/DataPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FloatingCTA } from "../../../components/ui/floating-cta";
 import { ContactFooterSection } from "./ContactFooterSection";
-import { DataMainContentSection } from "./DataMainContentSection";
+import { MainContentSection } from "./MainContentSection";
 import { ProjectCallToActionSection } from "./ProjectCallToActionSection";
 import DataHero from "./DataHero.tsx";
 
@@ -50,7 +50,7 @@ export const Home = (): JSX.Element => {
       <div className="relative z-10">
         <section className="w-full translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:0ms]">
           <DataHero />
-          <DataMainContentSection />
+          <MainContentSection />
         </section>
 
         <section className="w-full translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
